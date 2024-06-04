e106974afcf6daba852628448b79557a96d63392
Rafa Lima <132394137+rafalimma@users.noreply.github.com>
2024-06-04 13:53:17 -0300

    Add files via upload

---
+++ SQLexistis.sql
@@ -0,0 +1,19 @@
+-- EXISTS
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+-- se existe algum registro em uma subquery
+USE employees;
+
+SELECT * FROM salaries;
+
+SELECT last_name, (
+	SELECT SUM(salary)
+    FROM salaries
+    WHERE employees.emp_no = salaries.emp_no
+) AS soma_salario
+FROM employees
+WHERE EXISTS (
+	SELECT salary
+    FROM salaries
+    WHERE employees.emp_no = salaries.emp_no
+    HAVING SUM(salary) > 100000
+);
